81742e9d87612d2f9c0834dadb50d4b158a5556d
Lucas Carvalho <lucasbr023@gmail.com>
2018-12-06 15:50:32 -0300

    Part 8

---
+++ App07_08_09_ToDo/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App07_08_09_ToDo
+{
+    public class Person : INotifyPropertyChanged
+    {
+        private string fullName;
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set
+            {
+                fullName = value;
+                OnPropertyChanged("FullName");
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
