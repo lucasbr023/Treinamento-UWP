9e3d09a04311409b8b138737e30b2a39b2e1d5a8
Lucas Carvalho <lucasbr023@gmail.com>
2018-12-10 13:22:28 -0300

    App10AsyncTask

---
+++ App11/App.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Prism.Unity.Windows;
+
+namespace App10AsyncTask
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : PrismUnityApplication
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override Task OnInitializeAsync(IActivatedEventArgs args)
+        {
+            return base.OnInitializeAsync(args);
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate("Main", null);
+            return Task.FromResult(true);
+        }
+
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
+
---
+++ App11/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+
+namespace App10AsyncTask.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+
+        #region Commands
+        public DelegateCommand StartCommand { get; set; }
+        public DelegateCommand CancelCommand { get; set; }
+        #endregion
+
+        #region Properties
+
+        private int _progressBarOneValue;
+
+        public int ProgressBarOneValue
+        {
+            get { return _progressBarOneValue; }
+            set
+            {
+                SetProperty(ref _progressBarOneValue, value);
+            }
+        }
+
+        private int _progressBarTwoValue;
+
+        public int ProgressBarTwoValue
+        {
+            get { return _progressBarTwoValue; }
+            set { SetProperty(ref _progressBarTwoValue, value); }
+        }
+
+        private int _progressBarThreeValue;
+
+        public int ProgressBarThreeValue
+        {
+            get { return _progressBarThreeValue; }
+            set { SetProperty(ref _progressBarThreeValue, value); }
+        }
+
+        public string ProgressBarOneText { get; set; }
+        public string ProgressBarTwoText { get; set; }
+        public string ProgressBarThreeText { get; set; }
+
+        public CancellationTokenSource CancellationTokenSource { get; set; }
+        #endregion
+
+        public bool RunTask;
+
+        public MainPageViewModel()
+        {
+            StartCommand = new DelegateCommand(Start, () => !RunTask);
+            CancelCommand = new DelegateCommand(Cancel, () => RunTask);
+        }
+
+        private void Cancel()
+        {
+            if (CancellationTokenSource != null)
+            {
+                CancellationTokenSource.Cancel();
+            }
+
+            RunTask = false;
+
+            StartCommand.RaiseCanExecuteChanged();
+            CancelCommand.RaiseCanExecuteChanged();
+
+        }
+
+        private async void Start()
+        {
+            try
+            {
+                CancellationTokenSource = new CancellationTokenSource();
+
+                RunTask = true;
+                StartCommand.RaiseCanExecuteChanged();
+                CancelCommand.RaiseCanExecuteChanged();
+
+                var tasks = new List<Task>();
+
+                tasks.AddRange(new List<Task>()
+                {
+                    InitializeProgressBar(ProgressBarOneText, new Progress<int>(p => ProgressBarOneValue = p), CancellationTokenSource.Token),
+                    InitializeProgressBar(ProgressBarTwoText, new Progress<int>(p => ProgressBarTwoValue = p), CancellationTokenSource.Token),
+                    InitializeProgressBar(ProgressBarThreeText, new Progress<int>(p => ProgressBarThreeValue = p), CancellationTokenSource.Token),
+                });
+
+                await Task.WhenAll(tasks);
+            }
+            catch (OperationCanceledException)
+            {
+
+            }
+            finally
+            {
+                RunTask = false;
+                StartCommand.RaiseCanExecuteChanged();
+                CancelCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        private async Task InitializeProgressBar(string valueText, IProgress<int> progress, CancellationToken token)
+        {
+            if (int.TryParse(valueText, out var time))
+            {
+                await Task.Run(async () =>
+                {
+                    progress.Report(0);
+                    for (int i = 0; i <= time; i++)
+                    {
+                        await Task.Delay(300, token);
+                        progress.Report(i);
+                        token.ThrowIfCancellationRequested();
+                    }
+                }, token);
+            }
+        }
+    }
+}
