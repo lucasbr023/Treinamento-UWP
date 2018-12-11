ded8661f2c947a40a72e2a33f6509c59f3adad9a
Lucas Carvalho <lucasbr023@gmail.com>
2018-12-11 10:12:56 -0300

    ChallengeCustomControlCounter

---
+++ ChallengeCustomControlCounter/Controls/CounterControl.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Windows.UI;
+using Windows.UI.Xaml;
+using ChallengeCustomControlCounter.ViewModels;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace ChallengeCustomControlCounter.Controls
+{
+    public sealed partial class CounterControl : UserControl
+    {
+        public CounterControlViewModel ViewModel { get; set; }
+        public CounterControl()
+        {
+            this.InitializeComponent();
+            PointValue = 0;
+            TextBlockPointValue.Text = PointValue.ToString();
+        }
+
+        private int pointValue;
+
+        public int PointValue
+        {
+            get { return pointValue; }
+            set
+            {
+                pointValue = value;
+                DefineColorPointText();
+            }
+        }
+
+
+        private void AddPoint(object sender, RoutedEventArgs e)
+        {
+            PointValue = PointValue + 1;
+            TextBlockPointValue.Text = PointValue.ToString();
+        }
+
+        private void RemovePoint(object sender, RoutedEventArgs e)
+        {
+            PointValue = PointValue - 1;
+            TextBlockPointValue.Text = PointValue.ToString();
+        }
+
+        private void DefineColorPointText()
+        {
+            if (PointValue < 0) 
+            {
+                TextBlockPointValue.Foreground = new SolidColorBrush(Colors.Red);
+            }else if (PointValue > 0)
+            {
+                TextBlockPointValue.Foreground = new SolidColorBrush(Colors.Green);
+            }
+            else
+            {
+                TextBlockPointValue.Foreground = new SolidColorBrush(Colors.Black);
+            }
+        }
+
+    }
+}
---
+++ ChallengeCustomControlCounter/ViewModels/CounterControlViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Commands;
+using Prism.Windows.Mvvm;
+
+namespace ChallengeCustomControlCounter.ViewModels
+{
+    public class CounterControlViewModel : ViewModelBase
+    {
+        #region Commands
+        public DelegateCommand AddPointCommand { get; set; }
+        public DelegateCommand RemovePointCommnad { get; set; }
+        #endregion
+
+        #region Properties
+
+        private int _pointValue;
+
+        public int PointValue
+        {
+            get { return _pointValue; }
+            set { SetProperty(ref _pointValue, value); }
+        }
+
+
+        #endregion
+
+        //public CounterControlViewModel()
+        //{
+        //    AddPointCommand = new DelegateCommand(AddPoint);
+        //    RemovePointCommnad = new DelegateCommand(RemovePoint);
+        //    PointValue = 0;
+        //}
+
+     
+    }
+}
